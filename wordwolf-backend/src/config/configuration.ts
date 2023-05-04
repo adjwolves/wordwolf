022b1876a140a57375b5a95153a84f14ef8f40d9
hiyoko3m <hiyoko3m.tech@gmail.com>
2023-05-04 16:46:23 +0900

    設定をしなかったときのバックエンドの起動ポートを3010に

--- wordwolf-backend/src/config/configuration.ts
+++ wordwolf-backend/src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT ?? "3000", 10) || 3000, // NestJSのサーバが起動するポート
+  port: parseInt(process.env.PORT ?? "3010", 10) || 3010, // NestJSのサーバが起動するポート
   dynamodb: {
     region: process.env.DYNAMODB_REGION ?? "ap-northeast-1",
     endpoint: process.env.DYNAMODB_ENDPOINT,

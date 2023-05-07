941ce7167bd121b141376324a64da76d6840770c
hiyoko3m <hiyoko3m.tech@gmail.com>
2023-05-07 13:29:55 +0900

    トップページのベースになるファイルの準備

---
+++ wordwolf-frontend/src/components/GameLayout.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import { ReactElement } from "react";
+import styles from "@/styles/GameLayout.module.css";
+
+type LayoutProps = Required<{
+  readonly children: ReactElement;
+}>;
+
+export default function GameLayout({ children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main}`}>
+        <div className={`${styles.gameArea}`}>
+          {children}
+        </div>
+      </main>
+    </>
+  );
+}
